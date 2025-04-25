0f260ff77e4cb49abe3a1a4713272b3542f8d57a
ChilamKatana <368903779@qq.com>
2025-04-25 18:11:23 +0800

    添加音频组件

---
+++ Packages/cn.etetet.yiuiaudio/Scripts/HotfixView/Client/YIUIInvokeLoadAudioClipHandler.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+namespace ET.Client
+{
+    [Invoke(EYIUIInvokeType.Sync)]
+    public class YIUIInvokeLoadAudioClipSyncHandler : AInvokeHandler<YIUIInvokeLoadAudioClip, AudioClip>
+    {
+        public override AudioClip Handle(YIUIInvokeLoadAudioClip args)
+        {
+            var resName = args.ResName;
+
+            if (YIUILoadComponent.Inst == null) return null;
+
+            #if UNITY_EDITOR
+            if (!YIUILoadComponent.Inst.VerifyAssetValidity(resName))
+            {
+                Log.Error($"验证资产有效性 没有这个资源 无法加载 请检查 {resName}");
+                return null;
+            }
+            #endif
+
+            var audioClip = YIUILoadComponent.Inst.LoadAsset<AudioClip>(resName);
+
+            if (audioClip == null)
+            {
+                Log.Error($"加载失败 没有这个资源 无法加载 请检查 {resName}");
+                return null;
+            }
+
+            return audioClip;
+        }
+    }
+
+    [Invoke(EYIUIInvokeType.Async)]
+    public class YIUIInvokeLoadAudioClipAsyncHandler : AInvokeHandler<YIUIInvokeLoadAudioClip, ETTask<AudioClip>>
+    {
+        public override async ETTask<AudioClip> Handle(YIUIInvokeLoadAudioClip args)
+        {
+            if (YIUILoadComponent.Inst == null) return null;
+            var resName = args.ResName;
+            #if UNITY_EDITOR
+            if (!YIUILoadComponent.Inst.VerifyAssetValidity(resName))
+            {
+                Log.Error($"验证资产有效性 没有这个资源 无法加载 请检查 {resName}");
+                return null;
+            }
+            #endif
+
+            var audioClip = await YIUILoadComponent.Inst.LoadAssetAsync<AudioClip>(resName);
+
+            if (audioClip == null)
+            {
+                Log.Error($"加载失败 没有这个资源 无法加载 请检查 {resName}");
+                return null;
+            }
+
+            return audioClip;
+        }
+    }
+}
